8fe24f0e539aaefba82737f70eabfdc33f8643ae
trimscash <dadeheye@gmail.com>
2023-09-23 16:58:17 +0900

    disable: session

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import session from 'express-session'
+// import session from 'express-session'
 import { get_todays_daily_element } from './util/get_daily_element'
 import answer from './route/answer'
 import hint from './route/hint'
@@ -8,22 +8,22 @@ import './cron/daily/daily_element_create_cron'
 const app = express()
 const port = 4000
 
-app.use(
-  session({
-    secret: Math.random().toString(36).slice(-8),
-    name: 'session',
-    resave: false,
-    rolling: true,
-    saveUninitialized: true,
-    cookie: {
-      path: '/',
-      httpOnly: true,
-      // secure: true,
-      maxAge: 180 * 1000,
-      // sameSite: 'none',
-    },
-  })
-)
+// app.use(
+//   session({
+//     secret: Math.random().toString(36).slice(-8),
+//     name: 'session',
+//     resave: false,
+//     rolling: true,
+//     saveUninitialized: true,
+//     cookie: {
+//       path: '/',
+//       httpOnly: true,
+//       // secure: true,
+//       maxAge: 180 * 1000,
+//       // sameSite: 'none',
+//     },
+//   })
+// )
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
