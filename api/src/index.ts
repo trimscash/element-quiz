a78b4bbb3cff22578d3cadf2a865304a85f554d6
trimscash <dadeheye@gmail.com>
2023-09-26 20:21:01 +0900

    change: cors

--- api/src/index.ts
+++ api/src/index.ts
@@ -26,7 +26,7 @@ const port = 4000
 // )
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.header('Access-Control-Allow-Origin', process.env.FRONT_URL)
   res.header('Access-Control-Allow-Credentials', 'true')
   res.header(
     'Access-Control-Allow-Headers',
